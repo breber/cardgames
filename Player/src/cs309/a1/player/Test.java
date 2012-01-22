45882059b85134479eb6852332b762def8db31fc
Jamie Kujawa <jgkujawa@iastate.edu>
2012-01-22 23:15:07 +0000

    This is a test with variables for a conflict.
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@8 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Player/src/cs309/a1/player/Test.java
+++ Player/src/cs309/a1/player/Test.java
@@ -1,5 +1,22 @@
 package cs309.a1.player;
 
 public class Test {
+	
+	private int card;
+	private int number;
+	
+	public int getCard() {
+		return card;
+	}
+	public void setCard(int card) {
+		this.card = card;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
 
 }
