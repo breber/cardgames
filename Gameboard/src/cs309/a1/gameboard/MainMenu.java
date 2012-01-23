fd30976dbeb771125961d01afacf235d01af7463
Brian Reber <breber@iastate.edu>
2012-01-23 00:54:53 +0000

    Add base activity so that we can see our layouts on actual devices
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@14 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Gameboard/src/cs309/a1/gameboard/MainMenu.java
@@ -0,0 +1,14 @@
+package cs309.a1.gameboard;
+
+import android.app.Activity;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		setContentView(R.layout.main);
+
+	}
+
+}
---
+++ Player/src/cs309/a1/player/MainMenu.java
@@ -0,0 +1,13 @@
+package cs309.a1.player;
+
+import android.app.Activity;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		setContentView(R.layout.main);
+	}
+
+}
