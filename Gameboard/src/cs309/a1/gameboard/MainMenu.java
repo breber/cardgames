ba6639b258e350e89f32266e63b91a55392f4837
Brian Reber <breber@iastate.edu>
2012-01-23 03:29:45 +0000

    Fix Main menu in Gameboard
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@23 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Gameboard/src/cs309/a1/gameboard/MainMenu.java
@@ -0,0 +1,46 @@
+package cs309.a1.gameboard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		setContentView(R.layout.main);
+
+		Button play = (Button) findViewById(R.id.btPlay);
+		Button about = (Button) findViewById(R.id.btAbout);
+		Button rules = (Button) findViewById(R.id.btRules);
+
+		rules.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent ruleButtonClick = new Intent(MainMenu.this, RulesActivity.class);
+				startActivity(ruleButtonClick);
+			}
+		});
+
+		about.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent aboutButtonClick = new Intent(MainMenu.this,	AboutActivity.class);
+				startActivity(aboutButtonClick);
+			}
+		});
+
+		play.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent playButtonClick = new Intent(MainMenu.this, ConnectActivity.class);
+				startActivity(playButtonClick);
+			}
+		});
+
+	}
+
+}
