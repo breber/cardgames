5ba32c1a8c48d9690b7fdea125ba4a33d5367190
Jamie Kujawa <jgkujawa@iastate.edu>
2012-01-23 02:21:58 +0000

    Set up the onClickListeners for the main menu to switch to the proper screen upon button click.
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@17 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Player/src/cs309/a1/player/MainMenu.java
+++ Player/src/cs309/a1/player/MainMenu.java
@@ -1,6 +1,10 @@
 package cs309.a1.player;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
 
@@ -8,6 +12,41 @@ public class MainMenu extends Activity {
 	protected void onStart() {
 		super.onStart();
 		setContentView(R.layout.main);
+
+		Button play = (Button) findViewById(R.id.btPlay);
+		Button about = (Button) findViewById(R.id.btAbout);
+		Button rules = (Button) findViewById(R.id.btRules);
+
+		rules.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent ruleButtonClick = new Intent(MainMenu.this,
+						RulesActivity.class);
+				startActivity(ruleButtonClick);
+			}
+		});
+
+		about.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent aboutButtonClick = new Intent(MainMenu.this,
+						AboutActivity.class);
+				startActivity(aboutButtonClick);
+			}
+		});
+
+		play.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent playButtonClick = new Intent(MainMenu.this,
+						PlayActivity.class);
+				startActivity(playButtonClick);
+			}
+		});
+
 	}
 
 }
