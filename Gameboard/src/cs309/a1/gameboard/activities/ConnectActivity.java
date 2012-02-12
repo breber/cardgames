26ae50095976d90eda2daa2bdac041b2f75d99d5
Brian Reber <breber@iastate.edu>
2012-02-12 21:41:12 +0000

    Add some basic flow in the Gameboard.
    Added a gameboard.xml and connect.xml layout
    
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@53 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Gameboard/src/cs309/a1/gameboard/activities/ConnectActivity.java
+++ Gameboard/src/cs309/a1/gameboard/activities/ConnectActivity.java
@@ -1,18 +1,26 @@
 package cs309.a1.gameboard.activities;
 
-import cs309.a1.gameboard.R;
 import android.app.Activity;
-import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import cs309.a1.gameboard.R;
 
 public class ConnectActivity extends Activity {
-//Josh was here
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-		setContentView(R.layout.information);
+		setContentView(R.layout.connect);
 
-		TextView title = (TextView) findViewById(R.id.informationTitle);
-		title.setText("Connect Activity");
+		Button connectButton = (Button) findViewById(R.id.connectButton);
+		connectButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(ConnectActivity.this, GameboardActivity.class);
+				startActivity(i);
+			}
+		});
 	}
-
 }
--- Gameboard/src/cs309/a1/gameboard/activities/GameboardActivity.java
+++ Gameboard/src/cs309/a1/gameboard/activities/GameboardActivity.java
@@ -1,8 +1,12 @@
 package cs309.a1.gameboard.activities;
 
-import cs309.a1.gameboard.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.widget.TextView;
+import cs309.a1.gameboard.R;
 
 public class GameboardActivity extends Activity {
 
@@ -13,6 +17,31 @@ public class GameboardActivity extends Activity {
 
 		TextView title = (TextView) findViewById(R.id.informationTitle);
 		title.setText("Gameboard");
+	}
+
+	@Override
+	public void onBackPressed() {
+		AlertDialog dlg = new AlertDialog.Builder(this).create();
+		dlg.setTitle(R.string.quit);
+		dlg.setButton(getResources().getString(R.string.yes), new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// Start the Main Menu
+				Intent intent = new Intent(GameboardActivity.this, MainMenu.class);
+				startActivity(intent);
+
+				// Finish this activity
+				finishActivity(RESULT_OK);
+			}
+		});
+		dlg.setButton2(getResources().getString(R.string.cancel), new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// Don't do anything if they cancel
+			}
+		});
+
+		dlg.show();
 	}
 
 }
