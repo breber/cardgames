5442716d8c092d420a9e7125f97a5924670ba400
Brian Reber <breber@iastate.edu>
2012-03-09 16:31:28 +0000

    Update all buttons to a custom Button that uses the Helsinki font
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@100 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Shared/src/cs309/a1/shared/Button.java
@@ -0,0 +1,22 @@
+package cs309.a1.shared;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+public class Button extends android.widget.Button {
+
+	private Context mContext;
+
+	public Button(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mContext = context;
+	}
+
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+		Typeface typeface = Typeface.createFromAsset(mContext.getAssets(), "helsinki.ttf");
+		setTypeface(typeface);
+	}
+}
