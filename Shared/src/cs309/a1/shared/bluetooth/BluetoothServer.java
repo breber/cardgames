c1201f7b0e13253f78458c8dc5a332472303bc98
Brian Reber <breber@iastate.edu>
2012-03-19 23:28:48 +0000

    Add a map of strings to BluetoothConnections
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@127 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Shared/src/cs309/a1/shared/bluetooth/BluetoothServer.java
+++ Shared/src/cs309/a1/shared/bluetooth/BluetoothServer.java
@@ -1,20 +1,23 @@
 package cs309.a1.shared.bluetooth;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class BluetoothServer extends BluetoothCommon {
 	private static BluetoothServer instance = null;
 
-	private ArrayList<BluetoothConnectionService> services;
+	private HashMap<String, BluetoothConnectionService> services;
 
 	private BluetoothServer() {
-		services = new ArrayList<BluetoothConnectionService>();
+		services = new HashMap<String, BluetoothConnectionService>();
 	}
 
 	public static BluetoothServer getInstance() {
 		if (instance == null) {
 			instance = new BluetoothServer();
 		}
+
 		return instance;
 	}
 
@@ -30,7 +33,7 @@ public class BluetoothServer extends BluetoothCommon {
 
 		int countConnected = 0;
 
-		for (BluetoothConnectionService service : services) {
+		for (BluetoothConnectionService service : services.values()) {
 			if (service.getState() == BluetoothConnectionService.STATE_CONNECTED) {
 				countConnected++;
 			}
@@ -39,19 +42,36 @@ public class BluetoothServer extends BluetoothCommon {
 		return countConnected;
 	}
 
+	/**
+	 * Gets a list of Devices ids
+	 * 
+	 * @return a list of connected devices' ids
+	 */
+	public List<String> getConnectedDevices() {
+		return new ArrayList<String>(services.keySet());
+	}
+
 
 	@Override
 	public boolean write(Object obj, String ... address) {
 		boolean retVal = true;
 
 		if (address.length == 0) {
-			for (BluetoothConnectionService service : services) {
+			for (BluetoothConnectionService service : services.values()) {
 				if (!performWrite(service, obj)) {
 					retVal = false;
 				}
 			}
 		} else {
+			for (String addr : address) {
+				BluetoothConnectionService service = services.get(addr);
 
+				if (service != null) {
+					if (!performWrite(service, obj)) {
+						retVal = false;
+					}
+				}
+			}
 		}
 
 		return retVal;
