c36dae08af78f306cfd964d451dec35e172e38dc
Josh Peters <j7peters@iastate.edu>
2012-03-20 00:28:14 +0000

    add ids for image views in order to be able to show if connected or not
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@134 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Gameboard/src/cs309/a1/gameboard/activities/ConnectActivity.java
+++ Gameboard/src/cs309/a1/gameboard/activities/ConnectActivity.java
@@ -2,14 +2,22 @@ package cs309.a1.gameboard.activities;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 import cs309.a1.gameboard.R;
+import cs309.a1.player.activities.ConnectActivity;
+import cs309.a1.player.activities.ShowCardsActivity;
 import cs309.a1.shared.Util;
+import cs309.a1.shared.activities.DeviceListActivity;
+import cs309.a1.shared.bluetooth.BluetoothConnectionService;
+import cs309.a1.shared.bluetooth.BluetoothConstants;
 import cs309.a1.shared.bluetooth.BluetoothServer;
 
 public class ConnectActivity extends Activity {
@@ -19,6 +27,36 @@ public class ConnectActivity extends Activity {
 	private Context mContext;
 	private BluetoothAdapter mBluetoothAdapter;
 	private BluetoothServer mBluetoothServer;
+	
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			int currentState = intent.getIntExtra(BluetoothConstants.STATE_MESSAGE_KEY, -1);
+
+			if (Util.isDebugBuild()) {
+				Toast.makeText(mContext, "onReceive " + currentState, Toast.LENGTH_LONG).show();
+			}
+
+			if (currentState == BluetoothConnectionService.STATE_CONNECTED) {
+				// We connected just fine, so bring them to the ShowCardsActivity, and close
+				// this activity out.
+				dlg.dismiss();
+				Intent showCards = new Intent(ConnectActivity.this, ShowCardsActivity.class);
+				startActivity(showCards);
+
+				ConnectActivity.this.setResult(RESULT_OK);
+				ConnectActivity.this.finish();
+			} else if (currentState == BluetoothConnectionService.STATE_LISTEN) {
+				// If we make it back to listening state, we weren't able to connect,
+				// so bring them back to the device list
+				dlg.dismiss();
+
+				Intent showDeviceList = new Intent(ConnectActivity.this, DeviceListActivity.class);
+				startActivityForResult(showDeviceList, DEVICE_LIST_RESULT);
+			}
+		}
+	};
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +66,8 @@ public class ConnectActivity extends Activity {
 		mContext = this;
 
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
+		registerReceiver(receiver, new IntentFilter(BluetoothConstants.STATE_CHANGE_INTENT));//TODO 
+		
 		if (!mBluetoothAdapter.isEnabled()) {
 			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
