375024f5e68c34430f5926e7189ed4c403f0a5e3
Brian Reber <breber@iastate.edu>
2012-03-20 00:22:53 +0000

    Move more constants into the BluetoothConstants
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@133 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Shared/src/cs309/a1/shared/bluetooth/BluetoothConstants.java
+++ Shared/src/cs309/a1/shared/bluetooth/BluetoothConstants.java
@@ -1,8 +1,15 @@
 package cs309.a1.shared.bluetooth;
 
+import java.util.UUID;
+
 public class BluetoothConstants {
 
+	public static final String DEVICE_CONNECTED_INTENT = "cs309.a1.shared.bluetooth.DEVICE_CONNECTED_CHANGE";
 	public static final String STATE_CHANGE_INTENT = "cs309.a1.shared.bluetooth.STATE_CHANGE";
+	public static final UUID MY_UUID = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+	// Name for the SDP record when creating server socket
+	public static final String SOCKET_NAME = "BluetoothChat";
 
 	public static final int READ_MESSAGE  = 0;
 	public static final int WRITE_MESSAGE = 1;
@@ -14,4 +21,10 @@ public class BluetoothConstants {
 	public static final String TOAST_MESSAGE_KEY = "TOAST_MESSAGE";
 	public static final String STATE_MESSAGE_KEY = "STATE";
 
+
+	// Constants that indicate the current connection state
+	public static final int STATE_NONE = 0;       // we're doing nothing
+	public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+	public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+	public static final int STATE_CONNECTED = 3;  // now connected to a remote device
 }
