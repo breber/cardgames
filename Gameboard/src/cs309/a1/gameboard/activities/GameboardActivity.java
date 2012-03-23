43d8a22ade941b56ce203dfc9d459f527ca2799a
Ashley Nelson <anelson6@iastate.edu>
2012-03-23 03:09:08 +0000

    Created gameboard layout and added 5 dummy cards to each player hand
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@177 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Gameboard/src/cs309/a1/gameboard/activities/GameboardActivity.java
+++ Gameboard/src/cs309/a1/gameboard/activities/GameboardActivity.java
@@ -7,10 +7,17 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import cs309.a1.crazyeights.CrazyEightGameRules;
 import cs309.a1.crazyeights.CrazyEightsTabletGame;
 import cs309.a1.gameboard.R;
+import cs309.a1.shared.Card;
 import cs309.a1.shared.Deck;
 import cs309.a1.shared.Game;
 import cs309.a1.shared.Player;
@@ -47,6 +54,33 @@ public class GameboardActivity extends Activity {
 		for(int i = 0; i < players.size(); i++){
 			bts.write(players.get(i), players.get(i).getId());
 		}
+		
+		placeCard(1, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(2, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(3, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(4, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(5, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(1, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(2, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(3, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(4, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(5, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(1, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(2, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(3, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(4, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(5, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(1, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(2, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(3, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(4, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(5, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(1, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(2, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(3, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(4, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(5, new Card(5, 0, R.drawable.back_blue_1, 54));
+		placeCard(0, new Card(0, 2, R.drawable.clubs_3, 2));
 	}
 
 	@Override
@@ -77,6 +111,62 @@ public class GameboardActivity extends Activity {
 		}
 
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	void placeCard(int location, Card newCard) {
+		
+		LinearLayout ll;
+		LinearLayout.LayoutParams lp;
+		
+		// convert dip to pixels
+		final float dpsToPixScale = getApplicationContext().getResources().getDisplayMetrics().density;
+		int pixels = (int) (125 * dpsToPixScale + 0.5f);
+		
+		// place in discard pile
+		if(location == 0) {
+			ImageView discard = (ImageView) findViewById(R.id.discardpile);
+			discard.setImageResource(newCard.getResourceId());
+		}
+		
+		// if Player 1 or Player 3
+		else if(location == 1 || location == 3) {
+			
+			if(location == 1) ll = (LinearLayout) findViewById(R.id.player1ll);
+			else ll = (LinearLayout) findViewById(R.id.player3ll);
+			
+			lp = new LinearLayout.LayoutParams(pixels, LinearLayout.LayoutParams.WRAP_CONTENT);
+			
+			ImageView toAdd = new ImageView(this);
+			toAdd.setImageResource(newCard.getResourceId());
+			toAdd.setId(newCard.getIdNum());
+			toAdd.setAdjustViewBounds(true);
+			ll.addView(toAdd, lp);
+		}
+		
+		// if Player 2 or Player 4
+		else if(location == 2 || location == 4) {
+			
+			if(location == 2) ll = (LinearLayout) findViewById(R.id.player2ll);
+			else ll = (LinearLayout) findViewById(R.id.player4ll);
+			
+			// rotate vertical card image 90 degrees
+			Bitmap verticalCard = BitmapFactory.decodeResource(getResources(), newCard.getResourceId());
+	        Matrix tempMatrix = new Matrix();
+	        tempMatrix.postRotate(90);
+	        Bitmap horCard = Bitmap.createBitmap(verticalCard, 0, 0, verticalCard.getWidth(), verticalCard.getHeight(), tempMatrix, true);
+	        
+	        ImageView toAdd = new ImageView(this);
+	        toAdd.setImageBitmap(horCard);
+	        
+	        lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, pixels);
+	        toAdd.setAdjustViewBounds(true);
+	        ll.addView(toAdd, lp);
+		}
+		
+		else {
+			ImageView draw = (ImageView) findViewById(R.id.drawpile);
+			draw.setImageResource(newCard.getResourceId());
+		}
 	}
 
 }
