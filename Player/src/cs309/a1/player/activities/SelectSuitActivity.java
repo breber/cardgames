d6e4aeda57df7230a02df515550c77ff1fa53b31
Jamie Kujawa <jgkujawa@iastate.edu>
2012-03-26 02:18:06 +0000

    Added a select suit dialog.
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@210 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Player/src/cs309/a1/player/activities/SelectSuitActivity.java
@@ -0,0 +1,15 @@
+package cs309.a1.player.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cs309.a1.player.R;
+
+public class SelectSuitActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.selectsuit);
+	}
+
+}
