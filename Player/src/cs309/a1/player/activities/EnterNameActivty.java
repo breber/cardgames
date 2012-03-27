e1c7e726142e2a7a8eae4ce46e351779f7276e2a
Jamie Kujawa <jgkujawa@iastate.edu>
2012-03-27 20:00:22 +0000

    Added an activity that will act as a pop-up for a user to enter their name.
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@231 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Player/src/cs309/a1/player/activities/EnterNameActivty.java
@@ -0,0 +1,18 @@
+package cs309.a1.player.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cs309.a1.player.R;
+
+public class EnterNameActivty extends Activity{
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.entername);
+	}
+
+}
