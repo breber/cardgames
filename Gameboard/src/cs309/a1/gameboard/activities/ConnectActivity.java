98ddc943d558d2da99f07070364e806b7ded14f4
Brian Reber <breber@iastate.edu>
2012-03-27 03:16:29 +0000

    Make buttons on player's hand turn black when it is not their turn, and then yellow when it is their turn.
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@225 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Gameboard/src/cs309/a1/gameboard/activities/ConnectActivity.java
+++ Gameboard/src/cs309/a1/gameboard/activities/ConnectActivity.java
@@ -73,6 +73,7 @@ public class ConnectActivity extends Activity {
 		tv.setText(getResources().getString(R.string.deviceName) + "\n" + mBluetoothAdapter.getName());
 
 		Button startButton = (Button) findViewById(R.id.startButton);
+		startButton.setEnabled(false);
 		startButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
