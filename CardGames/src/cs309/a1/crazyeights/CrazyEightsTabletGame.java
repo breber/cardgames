599e13b8ca7c77b24afbc3e5dca296cda30dd2ae
Jamie Kujawa <jgkujawa@iastate.edu>
2012-04-15 20:49:34 +0000

    JavaDoc for the crazy eights tablet game class.
    git-svn-id: https://repo.cs.iastate.edu/coms309/A1/trunk@371 817968c2-256c-45f1-8ff6-e7619683c4bf

--- CardGames/src/cs309/a1/crazyeights/CrazyEightsTabletGame.java
+++ CardGames/src/cs309/a1/crazyeights/CrazyEightsTabletGame.java
@@ -17,19 +17,54 @@ import cs309.a1.shared.Player;
 import cs309.a1.shared.Rules;
 import cs309.a1.shared.Util;
 
+/**
+ * A class for keeping track of the logic and game state for the game type crazy eights
+ */
 public class CrazyEightsTabletGame implements Game {
+	
+	/**
+	 * A tag for the class name
+	 */
 	private static final String TAG = CrazyEightsTabletGame.class.getName();
 
+	/**
+	 * A variable for and instance of the crazy eights game type
+	 */
 	private static CrazyEightsTabletGame instance = null;
 
+	/**
+	 * A private variable for a list of players in the current game
+	 */
 	private List<Player> players;
+	
+	/**
+	 * A private variable representing the game deck for the crazy eights game
+	 */
 	private Deck gameDeck;
+	
+	/**
+	 * A private variable for the rules interface for the crazy eights game
+	 */
 	private Rules rules;
+	
+	/**
+	 * A private variable to represent the difficulty of computers in the current game
+	 */
 	private String computerDifficulty = Constants.EASY;
 
+	/**
+	 * An iterator for removing cards from the shuffled deck
+	 */
 	private Iterator<Card> iter;
 
+	/**
+	 * A list of all the cards in the shuffle deck
+	 */
 	private ArrayList<Card> shuffledDeck;
+	
+	/**
+	 * A list of all the cards in the discard pile
+	 */
 	private ArrayList<Card> discardPile;
 
 	/**
@@ -60,6 +95,14 @@ public class CrazyEightsTabletGame implements Game {
 		return instance;
 	}
 
+	/**
+	 * A constructor for the crazy eights game type. This constructor will initialize the all the variables
+	 * for a game of crazy eights including the rules, players, deck, shuffled deck pile and the discard pile.
+	 * 
+	 * @param players a list of Player objects in the current game
+	 * @param gameDeck a deck of cards to be used in the game crazy eights
+	 * @param rules a Rules object for the crazy eights game
+	 */
 	public CrazyEightsTabletGame(List<Player> players, Deck gameDeck, Rules rules) {
 		this.players = players;
 		this.gameDeck = gameDeck;
@@ -69,53 +112,112 @@ public class CrazyEightsTabletGame implements Game {
 	}
 
 	@Override
+	/**
+	 * This method is a getter for the players list
+	 * 
+	 * @return a list of Player objects in the game
+	 */
 	public List<Player> getPlayers() {
 		return players;
 	}
 
+	/**
+	 * This method is a setter for the list of players in the current game
+	 * 
+	 * @param players a list of players that will be playing the game
+	 */
 	public void setPlayers(List<Player> players) {
 		this.players = players;
 	}
 
+	/**
+	 * This method is a getter for the deck object
+	 * 
+	 * @return a Deck object for the game type of crazy eights
+	 */
 	public Deck getGameDeck() {
 		return gameDeck;
 	}
 
+	/**
+	 * This method is a setter for the game deck to be used with the game
+	 * 
+	 * @param gameDeck a Deck object to represent the cards in the crazy eights game
+	 */
 	public void setGameDeck(Deck gameDeck) {
 		this.gameDeck = gameDeck;
 	}
 
+	/**
+	 * This method is a getter for the discard pile list
+	 * 
+	 * @return a list of Card objects in the discard pile
+	 */
 	public List<Card> getDiscardPile() {
 		return discardPile;
 	}
 
+	/**
+	 * This method is a setter for the current discard pile
+	 * 
+	 * @param discardPile a list of Card objects currently in the discard pile
+	 */
 	public void setDiscardPile(ArrayList<Card> discardPile) {
 		this.discardPile = discardPile;
 	}
 
+	/**
+	 * This method is a getter for the current game's Rules object
+	 * 
+	 * @return a Rules object to represent the current rules of the game
+	 */
 	public Rules getRules() {
 		return rules;
 	}
 
+	/**
+	 * This method is a setter for the Rules object
+	 * 
+	 * @param rules a Rules object from the rules interface to be used with the current game
+	 */
 	public void setRules(Rules rules) {
 		this.rules = rules;
 	}
 
-
+	/**
+	 * This method is a getter for the shuffled deck pile
+	 * 
+	 * @return a list of Card objects representing the shuffled deck
+	 */
 	public ArrayList<Card> getShuffledDeck() {
 		return shuffledDeck;
 	}
 
+	/**
+	 * This method is a setter for the shuffled deck
+	 * 
+	 * @param shuffledDeck an array list of cards to represent the shuffled deck
+	 */
 	public void setShuffledDeck(ArrayList<Card> shuffledDeck) {
 		this.shuffledDeck = shuffledDeck;
 	}
 
 	@Override
+	/**
+	 * This method is a setter for the computer difficulty
+	 * 
+	 * @param diff the difficulty of the computers for the current game
+	 */
 	public void setComputerDifficulty(String diff){
 		this.computerDifficulty = diff;
 	}
 
 	@Override
+	/**
+	 * This method is a getter for the computer difficulty variable
+	 * 
+	 * @return a String representing the difficulty of the computers in the game
+	 */
 	public String getComputerDifficulty(){
 		return this.computerDifficulty;
 	}
@@ -228,6 +330,7 @@ public class CrazyEightsTabletGame implements Game {
 
 	/**
 	 * This method allows a player to discard a card object on the discard pile
+	 * 
 	 * @param player the player who is going to make the discard
 	 * @param card the card the player chooses to discard
 	 */
@@ -239,6 +342,7 @@ public class CrazyEightsTabletGame implements Game {
 
 	/**
 	 * This method will return true if the player has run out of cards.
+	 * 
 	 * @param player the player to check
 	 * @return true if the player has 0 cards and false otherwise
 	 */
@@ -254,7 +358,9 @@ public class CrazyEightsTabletGame implements Game {
 
 	/**
 	 * This method will allow the player to draw a card from the draw pile
+	 * 
 	 * @param player the player who chooses to draw the card
+	 * @return a Card object that has been drawn from the draw pile
 	 */
 	@Override
 	public Card draw(Player player) {
@@ -311,6 +417,7 @@ public class CrazyEightsTabletGame implements Game {
 
 	/**
 	 * This method will return the last card added to the discard pile
+	 * 
 	 * @return a Card object representing the last card added to the discard pile
 	 */
 	@Override
@@ -320,6 +427,7 @@ public class CrazyEightsTabletGame implements Game {
 
 	/**
 	 * This method will return the number of players in the present game
+	 * 
 	 * @return an integer representing the number of players
 	 */
 	@Override
