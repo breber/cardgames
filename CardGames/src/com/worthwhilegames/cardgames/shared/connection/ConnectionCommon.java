2f5b2eb36c629b4c483112d5e3e549cb5ae586c1
Brian Reber <reber.brian@gmail.com>
2012-08-12 20:00:17 -0500

    Fix potential force close
    On some devices a NetworkOnMainThreadException gets thrown when trying
    to send data over a socket on the main activity thread. Now this should
    be handled on a new thread.

--- CardGames/src/com/worthwhilegames/cardgames/shared/connection/ConnectionCommon.java
+++ CardGames/src/com/worthwhilegames/cardgames/shared/connection/ConnectionCommon.java
@@ -15,7 +15,7 @@ public abstract class ConnectionCommon {
 	 * @param obj the object to write
 	 * @return whether the message was written or not
 	 */
-	protected boolean performWrite(ConnectionService service, int messageType, Object obj) {
+	protected boolean performWrite(final ConnectionService service, int messageType, final Object obj) {
 		// If we aren't connected, return false
 		if (service.getState() != ConnectionConstants.STATE_CONNECTED) {
 			return false;
@@ -29,19 +29,33 @@ public abstract class ConnectionCommon {
 			//		"DATA"		: obj.toString()
 			// }
 
-			JSONObject json = new JSONObject();
+			final JSONObject json = new JSONObject();
 			if (obj != null) {
 				json.put(ConnectionConstants.KEY_MSG_DATA, obj.toString());
 			}
 			json.put(ConnectionConstants.KEY_MESSAGE_TYPE, messageType);
 
-			service.write(json.toString().getBytes());
+			// Perform the networking stuff on a separate thread
+			// due to some devices throwing a NetworkOnMainThreadException
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					service.write(json.toString().getBytes());
+				}
+			}).start();
 		} catch (JSONException e) {
 			// If we encounter a JSON error, just send the message without the
 			// message type parameter
 			e.printStackTrace();
 
-			service.write(obj.toString().getBytes());
+			// Perform the networking stuff on a separate thread
+			// due to some devices throwing a NetworkOnMainThreadException
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					service.write(obj.toString().getBytes());
+				}
+			}).start();
 		}
 
 		return true;
